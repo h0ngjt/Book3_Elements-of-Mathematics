2619830f470378df2bc37ebc99351bec2f8bfbdb
h0ngjt <honjt@hotmail.com>
2023-10-22 12:58:56 +0800

    correct by flare8

--- Book3_Ch01_Python_Codes/Bk3_Ch1_01.py
+++ Book3_Ch01_Python_Codes/Bk3_Ch1_01.py
@@ -11,10 +11,10 @@
 import math
 
 # Print the value of pi
-print ('pi = ', math.pi)
+print('pi = ', math.pi)
 
 # Print the value of e
-print ('e = ', math.e)
+print('e = ', math.e)
 
 # Print the value of square root of 2
-print ('sqrt(2) = ', math.sqrt(2))
+print('sqrt(2) = ', math.sqrt(2))
